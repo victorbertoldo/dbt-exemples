ed22c832549d3780461e43451946fe7038301698
victor.bertoldo <eu_viktor@hotmail.com>
2021-11-29 08:24:12 -0300

    corrigindo problemas com dados incrementais

--- dbt-test/models/example/dates_inc.sql
+++ dbt-test/models/example/dates_inc.sql
@@ -2,8 +2,8 @@
 
 select *
 from "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF10TCL"."DATE_DIM"
-where d_date < current_date
+where d_date <= current_date
 
 {% if is_incremental() %}
-    and d_date> (select max(d_date) from {{ this}})
+    and d_date > (select max(d_date) from {{ this }})
 {% endif %}
--- dbt-test/models/example/orders_ord_by_date_inc.sql
+++ dbt-test/models/example/orders_ord_by_date_inc.sql
@@ -1,18 +1,28 @@
 {{ config(materialized='incremental', unique_key='id') }}
 
-select ROW_NUMBER() OVER (Order by o_orderpriority || '|' || l_shipmode) as id
-,* from (
-select    
-    o.o_orderpriority,
-    i.l_shipmode,
-    sum(i.l_tax) as taxes
-from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS" o
-inner join "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."LINEITEM" i
-on o.o_orderkey = i.l_orderkey
-group by 
-    o.o_orderpriority,
-    i.l_shipmode)
+with aggr as (
+select ROW_NUMBER() OVER (Order by a.id) as id,
+    a.o_orderpriority,
+    a.l_shipmode,
+    a.taxes
+from (
+    select 
+        (o.o_orderpriority || '|' ||  i.l_shipmode) as id,
+        o.o_orderpriority,
+        i.l_shipmode,
+        sum(i.l_tax) as taxes
+    from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS" o
+    inner join "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."LINEITEM" i
+    on o.o_orderkey = i.l_orderkey
+    group by 
+        (o.o_orderpriority || '|' ||  i.l_shipmode),
+        o.o_orderpriority,
+        i.l_shipmode) a
+    
+)    
+select *
+from aggr
 
 {% if is_incremental() %}
-    and id> (select max(id) from {{ this}})
+    where id > (select max(id) from {{ this}})
 {% endif %}    
