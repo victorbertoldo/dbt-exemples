abbb01deedd3b98479d968661d459abafeca606a
victor.bertoldo <eu_viktor@hotmail.com>
2021-12-10 17:19:55 -0300

    .

---
+++ dbt-test/tests/assert_under_10_percent_null.sql
@@ -0,0 +1,8 @@
+﻿-- this query will verify the percentage of null values on a field table
+
+
+SELECT
+    sum(case when id is null then 1 else 0 end) / count(*) as total_nulls
+FROM {{ref('test_first_dbt_model')}}
+
+having sum(case when id is null then 1 else 0 end) / count(*) <= 0.1
