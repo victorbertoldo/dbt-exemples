2d73fbe5726ac8a1d1097c0c978d0d49c71a64d5
victorbertoldo_digio <eu_viktor@hotmail.com>
2023-02-27 18:51:53 -0300

    teste

---
+++ macros/for_if_statement.sql
@@ -0,0 +1,16 @@
+﻿{% set foods = ['carrot', 'hotdog', 'cucumber', 'bell pepper'] %}
+
+{% for food in foods %}
+    {% if food == 'hotdog' %}
+        {% set food_type = 'snack' %}
+    {% else %}
+        {% set food_type = 'vegetable' %}
+    {% endif %}
+
+The humble {{ food }} is my favorite {{ food_type }}
+
+{% endfor %}
+
+-- You will see that in the output there is a bunch of whitespaces and newlines, because jinja considers each line of code as a new line on output. 
+-- So to correct that we can add a minus sine inside of de {% ... %}, wold be like this: {%- ... -%}
+-- This will remove the whitespaces and newlines from the output.
---
+++ macros/lists.sql
@@ -0,0 +1,9 @@
+﻿{% set my_animals = ['lemur', 'wolf', 'panther', 'tardigrade']%}
+
+--  {{ my_animals[0]}}
+
+{% for animal in my_animals %}
+
+    My favorite animal is the {{ animal }}
+
+{% endfor %}
